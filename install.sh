2456eecd954df0910a7d7a4609725f779adffe3a
kumaotto <won.artoiler09@icloud.com>
2022-06-04 23:39:26 +0900

    add test yml

--- install.sh
+++ install.sh
@@ -1,9 +1,4 @@
-# ansibleEc2
-
-first, install Ansible
-```
 sudo apt update
 sudo apt install software-properties-common
 sudo apt-add-repository --yes --update ppa:ansible/ansible
 sudo apt install ansible
-```
